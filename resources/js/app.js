517deff773c210df764cea1d8059eb998651c0c7
mohwan <chaoui.mar2@gmail.com>
2021-08-07 16:09:19 +0100

    echo server & basic readme

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+
+// laravel-echo-server
+window.io = require('socket.io-client');
+import Echo from 'laravel-echo';
+
+window.Echo = new Echo({
+    namespace: 'app\\Events',
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':6001',
+    auth: {
+        headers: {
+            Authorization: 'Bearer ' + window.csrf_token,
+        }
+    }
+});
